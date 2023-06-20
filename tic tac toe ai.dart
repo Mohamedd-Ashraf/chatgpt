178d4349e10ebdebb811bcbe129257e7e0c77956
Mohamedd-Ashraf <110347603+Mohamedd-Ashraf@users.noreply.github.com>
2023-06-21 00:11:56 +0300

    Update tic tac toe ai.dart

--- tic tac toe ai.dart
+++ tic tac toe ai.dart
@@ -1,226 +1,136 @@
- Widget getSmallBoard(int parent) {
-    if (!completeBlock[parent] || ultimateParentBoard[parent] == '') {
-      return GridView(
-          gridDelegate:
-              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-          children: <Widget>[
-            GestureDetector(
-              onTap: () {
-                _tapped(parent, 0);
-              },
-              child: Container(
-                color: !availableBlock[parent]
-                    ? Color(0xFFB388FF)
-                    : Colors.grey[300],
-                margin: availableBlock[parent]
-                    ? EdgeInsets.all(1.0)
-                    : EdgeInsets.all(2.0),
-                child: Center(
-                  child: Text(
-                    boards[parent][0].toString(),
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      fontFamily: 'Redressed',
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                _tapped(parent, 1);
-              },
-              child: Container(
-                color: !availableBlock[parent]
-                    ? Color(0xFFB388FF)
-                    : Colors.grey[300],
-                margin: availableBlock[parent]
-                    ? EdgeInsets.all(1.0)
-                    : EdgeInsets.all(2.0),
-                child: Center(
-                  child: Text(
-                    boards[parent][1].toString(),
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      fontFamily: 'Redressed',
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                _tapped(parent, 2);
-              },
-              child: Container(
-                color: !availableBlock[parent]
-                    ? Color(0xFFB388FF)
-                    : Colors.grey[300],
-                margin: availableBlock[parent]
-                    ? EdgeInsets.all(1.0)
-                    : EdgeInsets.all(2.0),
-                child: Center(
-                  child: Text(
-                    boards[parent][2].toString(),
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      fontFamily: 'Redressed',
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                _tapped(parent, 3);
-              },
-              child: Container(
-                color: !availableBlock[parent]
-                    ? Color(0xFFB388FF)
-                    : Colors.grey[300],
-                margin: availableBlock[parent]
-                    ? EdgeInsets.all(1.0)
-                    : EdgeInsets.all(2.0),
-                child: Center(
-                  child: Text(
-                    boards[parent][3].toString(),
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      fontFamily: 'Redressed',
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                _tapped(parent, 4);
-              },
-              child: Container(
-                color: !availableBlock[parent]
-                    ? Color(0xFFB388FF)
-                    : Colors.grey[300],
-                margin: availableBlock[parent]
-                    ? EdgeInsets.all(1.0)
-                    : EdgeInsets.all(2.0),
-                child: Center(
-                  child: Text(
-                    boards[parent][4].toString(),
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      fontFamily: 'Redressed',
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                _tapped(parent, 5);
-              },
-              child: Container(
-                color: !availableBlock[parent]
-                    ? Color(0xFFB388FF)
-                    : Colors.grey[300],
-                margin: availableBlock[parent]
-                    ? EdgeInsets.all(1.0)
-                    : EdgeInsets.all(2.0),
-                child: Center(
-                  child: Text(
-                    boards[parent][5].toString(),
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      fontFamily: 'Redressed',
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                _tapped(parent, 6);
-              },
-              child: Container(
-                color: !availableBlock[parent]
-                    ? Color(0xFFB388FF)
-                    : Colors.grey[300],
-                margin: availableBlock[parent]
-                    ? EdgeInsets.all(1.0)
-                    : EdgeInsets.all(2.0),
-                child: Center(
-                  child: Text(
-                    boards[parent][6].toString(),
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      fontFamily: 'Redressed',
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                _tapped(parent, 7);
-              },
-              child: Container(
-                color: !availableBlock[parent]
-                    ? Color(0xFFB388FF)
-                    : Colors.grey[300],
-                margin: availableBlock[parent]
-                    ? EdgeInsets.all(1.0)
-                    : EdgeInsets.all(2.0),
-                child: Center(
-                  child: Text(
-                    boards[parent][7].toString(),
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      fontFamily: 'Redressed',
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                _tapped(parent, 8);
-              },
-              child: Container(
-                color: !availableBlock[parent]
-                    ? Color(0xFFB388FF)
-                    : Colors.grey[300],
-                margin: availableBlock[parent]
-                    ? EdgeInsets.all(1.0)
-                    : EdgeInsets.all(2.0),
-                child: Center(
-                  child: Text(
-                    boards[parent][8].toString(),
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      fontFamily: 'Redressed',
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
-          ]);
-    } else {
-      return Center(
-        child: Text(
-          ultimateParentBoard[parent],
-          style: TextStyle(
-            fontSize: 100.0,
-            fontFamily: 'Redressed',
-          ),
-        ),
-      );
+function game(){
+    //BG FILL
+    ctx.fillStyle = COLORS.white;
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    ctx.lineWidth = 3;
+    var squareSize = WIDTH/4;
+
+    //AI HANDLER
+
+    if(currentTurn === -1 && gameRunning && AIACTIVE){
+
+        console.log("Start AI");
+        //document.getElementById("loader").removeAttribute('hidden');
+
+        bestMove = -1;
+        bestScore = [-Infinity, -Infinity, -Infinity, -Infinity, -Infinity, -Infinity, -Infinity, -Infinity, -Infinity];
+
+        RUNS = 0; //Just a variable where I store how many times minimax has run
+
+        //Calculates the remaining amount of empty squares
+        var count = 0;
+        for(var bt = 0; bt < boards.length; bt++){
+            if(checkWinCondition(boards[bt]) === 0){
+                boards[bt].forEach((v) => (v === 0 && count++));
+            }
+        }
+
+
+        if(currentBoard === -1 || checkWinCondition(boards[currentBoard]) !== 0){
+            var savedMm;
+
+            console.log("Remaining: " + count);
+
+            //This minimax doesn't actually play a move, it simply figures out which board you should play on
+            if(MOVES < 10) {
+                savedMm = miniMax(boards, -1, Math.min(4, count), -Infinity, Infinity, true); //Putting math.min makes sure that minimax doesn't run when the board is full
+            }else if(MOVES < 18){
+                savedMm = miniMax(boards, -1, Math.min(5, count), -Infinity, Infinity, true);
+            }else{
+                savedMm = miniMax(boards, -1, Math.min(6, count), -Infinity, Infinity, true);
+            }
+            console.log(savedMm.tP);
+            currentBoard = savedMm.tP;
+        }
+
+        //Just makes a quick default move for if all else fails
+        for (var i = 0; i < 9; i++) {
+            if (boards[currentBoard][i] === 0) {
+                bestMove = i;
+                break;
+            }
+        }
+
+
+        if(bestMove !== -1) { //This condition should only be false if the board is full, but it's here in case
+
+            //Best score is an array which contains individual scores for each square, here we're just changing them based on how good the move is on that one local board
+            for (var a = 0; a < 9; a++) {
+                if (boards[currentBoard][a] === 0) {
+                    var score = evaluatePos(boards[currentBoard], a, currentTurn)*45;
+                    bestScore[a] = score;
+                }
+            }
+
+            //And here we actually run minimax and add those values to the array
+            for(var b = 0; b < 9; b++){
+                if(checkWinCondition(boards[currentBoard]) === 0){
+                    if (boards[currentBoard][b] === 0) {
+                        boards[currentBoard][b] = ai;
+                        var savedMm;
+                        //Notice the stacking, at the beginning of the game, the depth is much lower than at the end
+                        if(MOVES < 20){
+                            savedMm = miniMax(boards, b, Math.min(5, count), -Infinity, Infinity, false);
+                        }else if(MOVES < 32){
+                            console.log("DEEP SEARCH");
+                            savedMm = miniMax(boards, b, Math.min(6, count), -Infinity, Infinity, false);
+                        }else{
+                            console.log("ULTRA DEEP SEARCH");
+                            savedMm = miniMax(boards, b, Math.min(7, count), -Infinity, Infinity, false);
+                        }
+                        console.log(savedMm);
+                        var score2 = savedMm.mE;
+                        boards[currentBoard][b] = 0;
+                        bestScore[b] += score2;
+                        //boardSel[b] = savedMm.tP;
+                        //console.log(score2);
+                    }
+                }
+            }
+
+            console.log(bestScore);
+
+            //Choses to play on the square with the highest evaluation in the bestScore array
+            for(var i in bestScore){
+                if(bestScore[i] > bestScore[bestMove]){
+                    bestMove = i;
+                }
+            }
+
+            //Actually places the cross/nought
+            if(boards[currentBoard][bestMove] === 0){
+                boards[currentBoard][bestMove] = ai;
+                currentBoard = bestMove;
+            }
+
+            console.log(evaluateGame(boards, currentBoard));
+        }
+
+        //document.getElementById("loader").setAttribute('hidden', 'hidden');
+
+        currentTurn = -currentTurn;
+
     }
-  }
+
+    shapeSize = squareSize/6;
+
+    //mouseClickHandler
+    if(clicked === true && gameRunning) {
+        for (var i in boards) {
+            if(currentBoard !== -1){i = currentBoard;if(mainBoard[currentBoard] !== 0){continue;}}
+            for (var j in boards[i]) {
+                if(boards[i][j] === 0) {
+                    if (mousePosX > (WIDTH / 3 - squareSize) / 2 + squareSize / 6 - shapeSize + (j % 3) * squareSize / 3 + (i % 3) * WIDTH / 3 && mousePosX < (WIDTH / 3 - squareSize) / 2 + squareSize / 6 + shapeSize + (j % 3) * squareSize / 3 + (i % 3) * WIDTH / 3) {
+                        if (mousePosY > (WIDTH / 3 - squareSize) / 2 + squareSize / 6 - shapeSize + Math.floor(j / 3) * squareSize / 3 + Math.floor(i / 3) * WIDTH / 3 && mousePosY < (WIDTH / 3 - squareSize) / 2 + squareSize / 6 + shapeSize + Math.floor(j / 3) * squareSize / 3 + Math.floor(i / 3) * WIDTH / 3) {
+                            boards[i][j] = currentTurn;
+                            currentBoard = j;
+                            currentTurn = -currentTurn;
+                            MOVES++;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
